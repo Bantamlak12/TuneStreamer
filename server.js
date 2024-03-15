41e381a41c126f3ea313ac2fe80f010e236a7e7d
Bantamlak <bantamlak29@gmail.com>
2024-03-15 14:52:01 +0300

    starting to use session

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const session = require('express-session');
-const morgan = require('morgan');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require("express");
+const session = require("express-session");
+const morgan = require("morgan");
+const path = require("path");
+const dotenv = require("dotenv");
 
-const routes = require('./routes');
+const routes = require("./routes");
+const { dbClient, sessionStore } = require("./utils/db");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
 
 /************************************************/
 //  1. Express Setup
@@ -15,18 +16,37 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 /************************************************/
+//   2. Database connection
+/************************************************/
+dbClient.isAlive();
+/************************************************/
 //   3. Middleware Configuration
 /************************************************/
-
 // MORGAN MIDDLEWARE FOR LOGGING A REQUEST IN A FORMATTED WAY.
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 // MIDDLEWARE
+
 app.use(express.json());
 // SET THE VIEW  ENGINE TO USE EJS
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+// STATIC FILES LOCATION
+app.use(express.static(path.join(__dirname, "public")));
+// SESSION MIDDLEWARE
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+      secure: false, // It will be changed to true in production
+      httpOnly: true,
+    },
+  })
+);
 // LOAD ALL ROUTES  FROM THE /ROUTES/INDEX.JS FILE
 app.use(routes);
 

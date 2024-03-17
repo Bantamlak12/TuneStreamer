d244120216fd772aa3830232ac112f13b8683f67
Bantamlak <bantamlak29@gmail.com>
2024-03-17 14:08:45 +0300

    new route added to delete all users

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const AuthController = require('../controllers/AuthController');
+const UserController = require('../controllers/UserController');
 const {
   useRequireAuth,
   adminRequireAuth,
@@ -15,5 +16,6 @@ router.post('/signout', AuthController.signout);
 router.get('/dashboard', useRequireAuth, AuthController.getDashboard);
 router.get('/admin/musics', adminRequireAuth, AuthController.getMusicsPage);
 router.get('/admin/users', adminRequireAuth, AuthController.getUsersPage);
+router.delete('/users', adminRequireAuth, UserController.deleteAllUsers);
 
 module.exports = router;

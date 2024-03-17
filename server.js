f05ab9c2ec6d64ed3531911abc79e333c7b84051
Bantamlak <bantamlak29@gmail.com>
2024-03-17 10:37:17 +0300

    google authentication implemented

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const session = require('express-session');
 const morgan = require('morgan');
 const path = require('path');
 const dotenv = require('dotenv');
+const passport = require('passport');
 
 const routes = require('./routes');
 const { dbClient, sessionStore } = require('./utils/db');
+const User = require('./models/User');
 
 dotenv.config({ path: './config.env' });
 
@@ -13,7 +15,7 @@ dotenv.config({ path: './config.env' });
 //  1. Express Setup
 /************************************************/
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 /************************************************/
 //   2. Database connection
@@ -53,3 +55,75 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`Express app is running on port: ${PORT}`);
 });
+
+// PASSPORT SETUP
+let userProfile;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('view engine', 'ejs');
+
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('error', (req, res) => res.send('error logging in'));
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+// GOOGLE AUTH
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/auth/google/callback',
+    },
+    function (accessToken, refreshToken, profile, done) {
+      userProfile = profile;
+      return done(null, userProfile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  async function (req, res) {
+    try {
+      // Extract user data from the profile
+      const { id, name, emails } = userProfile;
+
+      // Check if the user exists in the database
+      const user = await User.findOne({ googleId: id });
+      if (!user) {
+        const newUser = new User({
+          googleId: id,
+          firstName: name.givenName,
+          lastName: name.familyName,
+          email: emails[0].value,
+        });
+
+        await newUser.save();
+      }
+
+      req.session.isAuthenticated = true;
+      res.redirect('/dashboard');
+    } catch (error) {
+      console.error(`Error saving user data: ${error.message}`);
+      res.redirect('/signin');
+    }
+  }
+);

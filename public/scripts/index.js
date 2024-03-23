cdc17c8fc296b273188ad40158e8e20464165a9f
Bantamlak <bantamlak29@gmail.com>
2024-03-23 10:53:40 +0300

    script updated

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,9 @@
 const toggleIcon = document.querySelector('.toggle-icon');
 const navLinks = document.querySelector('.nav-links');
 const html = document.querySelector('html');
+const header = document.querySelector('.header');
+const headerContainer = document.querySelector('.header-container');
+const headerLinks = document.querySelector('.header-links');
 
 const form = document.querySelector('form[name="contact-form"]');
 const btnFeedback = document.querySelector('.btn-feedback');
@@ -18,6 +21,15 @@ const setSuccess = (input) => {
   input.classList.remove('error');
 };
 
+const isValidEmail = (email) => {
+  const regexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+
+  const validated = regexp.test(email);
+
+  return validated;
+};
+
+// Toggle
 toggleIcon.addEventListener('click', (e) => {
   navLinks.classList.toggle('active');
 });
@@ -35,7 +47,7 @@ form.addEventListener('submit', function (e) {
     setSuccess(nameInput);
   }
 
-  if (!email) {
+  if (!email || !isValidEmail(email)) {
     setError(emailInput);
   } else {
     setSuccess(emailInput);
@@ -59,5 +71,43 @@ form.addEventListener('submit', function (e) {
       alert('Your feedback is submitted successfully. Thank you!');
       form.reset();
     });
+  }
+});
+/*************************************************/
+/* STICKY NAVIGATION: Intersection observer */
+/*************************************************/
+const headerHeignt = header.getBoundingClientRect().height;
+const callback = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+};
+
+const observer = new IntersectionObserver(callback, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headerHeignt}px`,
+});
+observer.observe(headerContainer);
+/************************************************************/
+/* SMOOTH SCROLLING: LOGO and NAVIGATIONS and THE BUTTON */
+/***********************************************************/
+headerLinks.addEventListener('click', function (e) {
+  const target = e.target;
+
+  if (target.classList.contains('signin-link')) {
+    return;
+  }
+
+  e.preventDefault();
+
+  const id = e.target.getAttribute('href');
+
+  if (e.target.classList.contains('nav-link')) {
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
